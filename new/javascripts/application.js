259ba8b4a97171aada16f817c83fb4d1f77a7a9f
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-07-29 15:39:40 -0500

    svg flash support

--- new/javascripts/application.js
+++ new/javascripts/application.js
@@ -1,2 +1,15 @@
 $(function(){
+  $('a').hover(function(){
+    $($('object', $(this))[0].contentDocument.documentElement).css("fill", "#000").css("cursor", "pointer"); 
+  }, function(){
+    $($('object', $(this))[0].contentDocument.documentElement).css("fill", "#bbb"); 
+  });
 });
+
+$(window).load(function(){
+  $('object').each(function(ind, obj){  
+    //$(obj).hover(function(){ console.log("hoverin")}, function(){$(obj).removeClass("black")});
+    
+    $(obj.contentDocument.documentElement).css("fill", "#bbb"); 
+  });
+})
