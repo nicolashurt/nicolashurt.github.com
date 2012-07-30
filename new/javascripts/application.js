f29fd342abc021ecd542827120016c20a4c4239c
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-07-29 19:49:13 -0500

    testing inline svg

--- new/javascripts/application.js
+++ new/javascripts/application.js
@@ -1,4 +1,6 @@
 $(function(){
+  $('svg').css("fill", "#ccc");
+  
   $('#menu a').hover(function(){
     $(this).css("color", "#000");
     $($('object', $(this))[0].contentDocument.documentElement).css("fill", "#000").css("cursor", "pointer"); 
@@ -12,6 +14,6 @@ $(window).load(function(){
   $('object').each(function(ind, obj){  
     //$(obj).hover(function(){ console.log("hoverin")}, function(){$(obj).removeClass("black")});
     
-    $(obj.contentDocument.documentElement).css("fill", "#aaa"); 
+    $(obj.contentDocument.documentElement).css("fill", "#aaa");
   });
 })
