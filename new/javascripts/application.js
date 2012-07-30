1f2cd9d9d73cccb7f4cbd9acac99392a8846425f
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-07-30 03:54:59 -0500

    slideshow

--- new/javascripts/application.js
+++ new/javascripts/application.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function(){  
     $('.description').each(function(ind, obj){
       $(obj).find('svg').css("fill", $(obj).data("fill"));
       $(obj).find('path').css("fill", $(obj).data("fill"));
