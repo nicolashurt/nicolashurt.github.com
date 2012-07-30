f7594fbbdcf8660d5b6fb08f1a9de9e18101a4bc
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-07-29 22:01:42 -0500

    bigger and color

--- new/javascripts/application.js
+++ new/javascripts/application.js
@@ -1,2 +1,7 @@
 $(function(){
+    $('.description').each(function(ind, obj){
+      $(obj).find('svg').css("fill", $(obj).data("fill"));
+      $(obj).find('path').css("fill", $(obj).data("fill"));
+      $(obj).find('g').css("fill", $(obj).data("fill"));
+    });
 });
