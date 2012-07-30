65cd4502bf73986254cafb58fd9717af4dbbc503
Christian Reyes <cjreyes@andrew.cmu.edu>
2012-07-29 20:04:43 -0500

    all inline svg

--- new/javascripts/application.js
+++ new/javascripts/application.js
@@ -1,19 +1,3 @@
 $(function(){
-  $('svg').css("fill", "#ccc");
   
-  $('#menu a').hover(function(){
-    $(this).css("color", "#000");
-    $($('object', $(this))[0].contentDocument.documentElement).css("fill", "#000").css("cursor", "pointer"); 
-  }, function(){
-    $(this).css("color", "#aaa");
-    $($('object', $(this))[0].contentDocument.documentElement).css("fill", "#aaa"); 
-  });
 });
-
-$(window).load(function(){
-  $('object').each(function(ind, obj){  
-    //$(obj).hover(function(){ console.log("hoverin")}, function(){$(obj).removeClass("black")});
-    
-    $(obj.contentDocument.documentElement).css("fill", "#aaa");
-  });
-})
