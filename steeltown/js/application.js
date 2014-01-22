af97b2ed168070f7a2bbdcab7a972db36559c29c
Christian Reyes <cjreyes@andrew.cmu.edu>
2014-01-21 22:00:20 -0600

    updated homepage, slideshow, director page, producer page

--- steeltown/js/application.js
+++ steeltown/js/application.js
@@ -3,5 +3,40 @@ $(function(){
     
     setTimeout(function(){
         $('#title').fadeIn(2000);
-    }, 1200)
-})
+    }, 1200);
+    
+    $('#nav a').click(function(){
+        var target = $(this).attr("href").split(".")[0]
+        moveTitleUp(target);
+        
+        $('#nav a').removeClass('current')
+        
+        $(this).addClass("current");
+        
+        return false;
+    });
+    
+    var moveTitleUp = function(section){
+        if(section == "index"){
+            $('#content').fadeOut(function(){
+                $('h1').animate({
+                    "margin-top": "200px",
+                    "font-size": "110px",
+                    "width": "600px" 
+                }); 
+            });
+        } else {
+            $('h1').animate({
+                "margin-top": 0,
+                "font-size": "60px",
+                "width": "350px" 
+            });   
+            
+            
+            $('#content').fadeOut(function(){
+                $('#content').html($('#' + section).html())
+                $('#content').fadeIn();
+            });
+        }
+    };
+});
